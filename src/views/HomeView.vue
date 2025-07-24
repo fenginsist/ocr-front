9f6e6981bbb16da208fac3ce0f4c6f4710bd8ca5
fengfanli <897961047@qq.com>
2025-07-24 09:20:12 +0800

    优化识别页面

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="page-container">
+    <!-- 头部 -->
     <header class="ocr-header">
       <div class="ocr-header-left">
         <img src="../assets/images/infor-logo.png" alt="logo" />
@@ -8,39 +9,55 @@
       <div class="ocr-header-right">
         <div class="el-dropdown">
           <div class="user-info">
-            <span class="avatar-user"><el-icon><UserFilled /></el-icon></span>
+            <span class="avatar-user"
+              ><el-icon><UserFilled /></el-icon
+            ></span>
             <span class="user-id">005878</span>
           </div>
         </div>
       </div>
     </header>
+    <!-- 下面：左右，两部分 -->
+
     <div class="ocr-container">
-      <el-upload
-        class="upload-area"
-        drag
-        action=""
-        :auto-upload="false"
-        :on-change="handleFileChange"
-      >
-        <i class="el-icon-upload"></i>
-        <div class="el-upload__text">
-          拖拽文件到这里，或 <em>点击上传</em>
+      <!-- 左 -->
+      <div class="ocr-sidebar">
+        <div>
+          <span>识别</span>
         </div>
-        <div class="el-upload__tip">支持 PDF、Word 等文档</div>
-      </el-upload>
+        <div>
+          <div>识别记录</div>
+          <div>收藏</div>
+        </div>
+      </div>
+      <!-- 右 -->
+      <div class="ocr-main">
+        <el-upload
+          class="upload-area"
+          drag
+          action=""
+          :auto-upload="false"
+          :on-change="handleFileChange"
+        >
+          <!-- <i class="el-icon-upload"></i> -->
+          <el-icon class="el-icon--upload"><upload-filled /></el-icon>
+          <div class="el-upload__text">拖拽文件到这里，或 <em>点击上传</em></div>
+          <div class="el-upload__tip">支持 PDF、Word 等文档</div>
+        </el-upload>
 
-      <el-button
-        class="submit-button"
-        type="primary"
-        :disabled="!selectedFile || loading"
-        @click="submitFile"
-      >
-        {{ loading ? '识别中...' : '点击识别' }}
-      </el-button>
+        <el-button
+          class="submit-button"
+          type="primary"
+          :disabled="!selectedFile || loading"
+          @click="submitFile"
+        >
+          {{ loading ? '识别中...' : '点击识别' }}
+        </el-button>
 
-      <div class="result-box">
-        <div class="result-text">
-          {{ resultText || '识别结果将显示在这里...' }}
+        <div class="result-box">
+          <div class="result-text">
+            {{ resultText || '识别结果将显示在这里...' }}
+          </div>
         </div>
       </div>
     </div>
@@ -48,6 +65,8 @@
 </template>
 
 <script setup>
+import { UploadFilled } from '@element-plus/icons-vue'
+
 import { ref } from 'vue'
 import axios from 'axios'
 
@@ -71,8 +90,8 @@ const submitFile = async () => {
   try {
     const response = await axios.post('/zcocr/get/file/content', formData, {
       headers: {
-        'Content-Type': 'multipart/form-data'
-      }
+        'Content-Type': 'multipart/form-data',
+      },
     })
     // console.log('11111',response)
     // console.log('22222',response.data)
@@ -88,19 +107,8 @@ const submitFile = async () => {
 </script>
 
 <style scoped>
-
-.page-container{
+.page-container {
   height: 100%;
-}
-
-.ocr-container {
-  max-width: 600px;
-  margin: 0px auto;
-  padding: 24px;
-  box-shadow: 0 4px 20px rgba(0, 0, 0, 0.1);
-  border-radius: 12px;
-  background-color: #fff;
-  text-align: center;
 }
 
 .ocr-header {
@@ -108,10 +116,10 @@ const submitFile = async () => {
   align-items: center;
   justify-content: space-between;
 
-  border-bottom: 1px solid rgba(219, 224, 232, .7);
+  border-bottom: 1px solid rgba(219, 224, 232, 0.7);
   height: 48px !important;
   background-color: #fff;
-  width: 100%;
+  /* width: 100%;  */ /* 放开这个，会导致左右出现滚动条  */
 
   padding: 0 20px;
 }
@@ -146,13 +154,41 @@ const submitFile = async () => {
   align-items: center;
 }
 
-.avatar-user, .user-id {
+.avatar-user,
+.user-id {
   margin-left: 10px;
 }
 
 .upload-area {
-  margin-bottom: 20px;
-  height: 100px;
+  display: flex;
+  flex-direction: column;
+  /* height: 100%; */ /* 加上这个和内部的（也就是下面的）height: 300px;冲突了  */
+}
+
+:deep(.el-upload-dragger) {
+  height: 300px;
+}
+
+.ocr-container {
+  display: flex;
+}
+
+.ocr-sidebar {
+  width: 30%;
+  height: 100%;
+}
+
+.ocr-main {
+  display: flex;
+  flex-direction: column; /** 上下排列 */
+
+  width: 100%;
+  /* margin: 0px auto; */
+  padding: 24px;
+  box-shadow: 0 4px 20px rgba(0, 0, 0, 0.1);
+  border-radius: 12px;
+  background-color: #fff;
+  text-align: center;
 }
 
 .submit-button {
@@ -163,7 +199,7 @@ const submitFile = async () => {
 }
 
 .result-box {
-  height: 200px;
+  height: 400px;
   padding: 12px;
   border: 1px solid #dcdfe6;
   border-radius: 8px;
